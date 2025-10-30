95777c1cabef2750c8dcbf4b391fef2c39b21db2
dilshanikatukurunda <dskwellala@gmail.com>
2025-10-30 23:57:47 +0530

    designed the board and added styles

--- src/Components/TicTacToe/TicTacToe.jsx
+++ src/Components/TicTacToe/TicTacToe.jsx
@@ -3,12 +3,28 @@ import './TicTacToe.css'
 import circle_icon from "../Assets/circle.png"
 import cross_icon from "../Assets/cross.png"    
 
+// let data = []
+
 const TicTacToe = () => {
   return (
     <div className='container'>
      <h1 className='title'>Tic Tac Toe in <span>React</span></h1>
      <div className='board'>
-
+       <div className='row1'>
+        <div className='boxes'></div>
+        <div className='boxes'></div>
+        <div className='boxes'></div>
+       </div> 
+       <div className='row2'>
+        <div className='boxes'></div>
+        <div className='boxes'></div>
+        <div className='boxes'></div>
+       </div> 
+       <div className='row3'>
+        <div className='boxes'></div>
+        <div className='boxes'></div>
+        <div className='boxes'></div>
+       </div> 
      </div>
      <button className='reset'>Reset</button> 
     </div>
